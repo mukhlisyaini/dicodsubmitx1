b5e953c24fb5b72381f2e978231e033f72bd65a6
mukhlisyaini <mukhlisyaini@gmail.com>
2020-08-20 01:13:05 +0800

    final

--- script/jsscript.js
+++ script/jsscript.js
@@ -3,8 +3,16 @@ const nav = document.querySelector('nav ul');
 const navspan1 = document.querySelector('nav div span');
 const navspan2 = document.querySelector('nav div span:nth-child(2)');
 const navspan3 = document.querySelector('nav div span:nth-child(3)');
+const menutoogle2 = document.querySelector('nav ul li a');
 
 menutoogle.addEventListener('click', function () {
+    navspan1.classList.toggle('animspan');
+    navspan2.classList.toggle('animspan2');
+    navspan3.classList.toggle('animspan3');
+    nav.classList.toggle('showslide');
+})
+
+menutoogle2.addEventListener('click', function () {
     nav.classList.toggle('showslide');
     navspan1.classList.toggle('animspan');
     navspan2.classList.toggle('animspan2');
